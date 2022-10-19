3f1fa4548ffe4a32fe7ab9af17f0fcb71c6ffc8f
BarefootDev <95222793+Barefoot-Dev@users.noreply.github.com>
2022-10-19 10:47:52 +0800

    Update setup.py
    Add formatter and requests

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version="0.0.1",
     packages=["discbot"],
     package_dir={"": "src"},
-    install_requires=["discord.py==2.0.1", "python-dotenv==0.21.0"],
+    install_requires=["discord.py==2.0.1", "python-dotenv==0.21.0", "requests==2.28.1", "black==22.10.0"],
 )
